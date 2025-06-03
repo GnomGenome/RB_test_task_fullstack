118a069d0bda3619fb4951a8db757d0dcb2c4fcd
GnomGenome <kolesnikovaam2003@gmail.com>
2025-06-04 02:09:49 +0300

    Всё работает, но карточки неправильные

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import './styles/main.scss';
 document.addEventListener("DOMContentLoaded", () => {
   const showVideoBtn = document.getElementById("show-video-btn");
   const videoContainer = document.getElementById("video-container");
+  const heroContainer = document.querySelector(".hero__container");
 
-  if (showVideoBtn && videoContainer) {
+  if (showVideoBtn && videoContainer && heroContainer) {
     showVideoBtn.addEventListener("click", () => {
-      videoContainer.classList.add("hero__video--active");
+      heroContainer.classList.add("hero__container--video-active");
       showVideoBtn.style.display = "none"; // спрячем кнопку
     });
   } else {
-    console.warn('Элемент showVideoBtn или videoContainer не найден в DOM');
+    console.warn('Один из элементов не найден: showVideoBtn, videoContainer или heroContainer');
   }
 });
